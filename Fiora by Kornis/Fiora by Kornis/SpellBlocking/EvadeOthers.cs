583c251e147d4e3df7a8821be91eb144a40aad22
Kornis <kornelija142@inbox.lt>
2017-09-16 06:17:36 +0900

    。

--- Fiora by Kornis/Fiora by Kornis/SpellBlocking/EvadeOthers.cs
+++ Fiora by Kornis/Fiora by Kornis/SpellBlocking/EvadeOthers.cs
@@ -172,7 +172,7 @@
                 Args.Target == null || string.IsNullOrEmpty(Args.SpellData.Name) ||
                 Args.SpellData.Name == "RenektonCleave" || Args.SpellData.Name == "AlistarE" ||
                 Args.SpellData.Name == "ChogathEAttack" ||
-                Args.SpellData.Name == "FerociousHowl" || Args.SpellData.Name == "NasusE" ||
+                Args.SpellData.Name == "FerociousHowl" || Args.SpellData.Name == "NasusE" || Args.SpellData.Name == "GarenQAttack" ||
                 Args.SpellData.Name.Contains("BasicAttack"))
             {
                 return;
