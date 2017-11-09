97196f79c570d77361b2fb83bd35053c4779d77f
Kornis <kornis188@yahoo.co.jp>
2017-11-09 22:01:08 +0900

    :>

--- Support AIO/Support AIO/Champions/Zyra.cs
+++ Support AIO/Support AIO/Champions/Zyra.cs
@@ -241,7 +241,7 @@ namespace Support_AIO.Champions
 
         protected override void LastHit()
         {
-            throw new NotImplementedException();
+         
         }
 
         public static readonly List<string> SpecialChampions = new List<string> { "Annie", "Jhin" };
