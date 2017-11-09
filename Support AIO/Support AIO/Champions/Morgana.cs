084c844cd5c2948f45f9cc420f20fd87883c971f
Kornis <kornis188@yahoo.co.jp>
2017-11-09 22:05:06 +0900

    :>

--- Support AIO/Support AIO/Champions/Morgana.cs
+++ Support AIO/Support AIO/Champions/Morgana.cs
@@ -133,7 +133,7 @@ namespace Support_AIO.Champions
 
         protected override void LastHit()
         {
-            throw new NotImplementedException();
+   
         }
 
         protected override void Drawings()
